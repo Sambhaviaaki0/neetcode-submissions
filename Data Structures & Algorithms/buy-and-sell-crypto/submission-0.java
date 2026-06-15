d2284c42af1bfee71c435d130b3ec36940a8dd04
Sambhaviaaki0 <23h41a4501@bvcits.edu.in>
2026-06-16 05:25:07 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int buy = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int i=0;i<n;i++) {
+            if(prices[i] < buy) {
+                buy = prices[i];
+            }
+            profit = Math.max(profit , prices[i]-buy);
+        }
+        return profit;
+    }
+}
